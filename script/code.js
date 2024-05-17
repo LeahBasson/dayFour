7d3a1adc942be2650d0ba059677e6ab09b7c0c43
LeahBasson <leahbasson54@gmail.com>
2024-05-17 15:54:21 +0200

    day four

---
+++ script/code.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'Leah',
+    age: 19
+};
+
+console.log(person.name);
+console.log(person.age);
+
+let person2 = ['Tamia',15];
+
+console.log(person2[0]);
+console.log(person2[1]);
+
